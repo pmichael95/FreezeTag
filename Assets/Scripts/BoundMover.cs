3930b90ef29e698dd88be82ddd283c839dbce5b5
Philip Michael <phil-mik@hotmail.com>
2018-01-23 15:30:15 -0500

    Initial push with basic project environment.

---
+++ Assets/Scripts/BoundMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundMover : MonoBehaviour {
+
+    // When any unit collides with the bounding walls, we move them to the opposite end appropriately
+    // Assumption: Only units can possibly collide with the bounds (they are the only possible collidable objects)
+    void OnTriggerEnter(Collider col) {
+        if(this.gameObject.tag == "Top & Bottom Bounds") {
+            float newZ = -col.gameObject.transform.position.z;
+            col.gameObject.transform.position = new Vector3(col.gameObject.transform.position.x, col.gameObject.transform.position.y, newZ);
+        }
+        else if(this.gameObject.tag == "Right & Left Bounds") {
+            float newX = -col.gameObject.transform.position.x;
+            col.gameObject.transform.position = new Vector3(newX, col.gameObject.transform.position.y, col.gameObject.transform.position.z);
+        }
+    }
+}
