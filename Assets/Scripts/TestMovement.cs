f1ad6a921886471ab17d0224bbe5e05aabc63ab9
Philip Michael <phil-mik@hotmail.com>
2018-01-23 16:00:45 -0500

    Slight adjustments and prefabing.

--- Assets/Scripts/TestMovement.cs
+++ Assets/Scripts/TestMovement.cs
@@ -14,6 +14,7 @@ public class TestMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
         if (Input.GetKey(KeyCode.W))
         {
             // Move forward
